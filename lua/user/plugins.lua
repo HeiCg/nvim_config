519db8db65a3269a0680ba86330b21a1e30e6365
Carlos Gustavo Arajo <carlosga@motorola.com>
2022-04-30 12:35:13 -0400

    Remove deprecated code from nvim-tree.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -22,6 +22,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "plugins.lua",
   group = group,
 })
+
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
@@ -40,11 +41,11 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim"  -- Have packer manage itself
-  use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim"   -- Useful lua functions used ny lots of plugins
-  use "windwp/nvim-autopairs"   -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim"   -- Easily comment stuff
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+  use "numToStr/Comment.nvim" -- Easily comment stuff
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
